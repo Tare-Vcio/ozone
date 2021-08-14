26c14b4b2b6e24b32b82eeab2b2f933cfcad772b
tarevcio <votrunghieu332001@gmail.com>
2021-08-14 17:31:01 +0700

    again

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+   
+    
+    $('.slider').slick({    
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots: true,
+    arrows: false,
+    autoplaySpeed: 3500,
+    speed: 1500,
+    });
+
+});
