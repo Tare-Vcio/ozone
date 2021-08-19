57faf83bd709a6e4828b3eac238eb8a424cbb295
tarevcio <votrunghieu332001@gmail.com>
2021-08-19 16:39:32 +0700

    again

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
     dots: true,
     arrows: false,
     autoplaySpeed: 3500,
@@ -13,3 +13,28 @@ $( document ).ready(function() {
     });
 
 });
+
+var wow = new WOW(
+    {
+      boxClass:     'wow',      // animated element css class (default is wow)
+      animateClass: 'animated', // animation css class (default is animated)
+      offset:       0,          // distance to the element when triggering the animation (default is 0)
+      mobile:       true,       // trigger animations on mobile devices (default is true)
+      live:         true,       // act on asynchronously loaded content (default is true)
+      callback:     function(box) {
+        // the callback is fired every time an animation is started
+        // the argument that is passed in is the DOM node being animated
+      },
+      scrollContainer: null // optional scroll container selector, otherwise use window
+    }
+  );
+  wow.init();
+
+  const sub_menu = document.querySelector('.sub-menu');
+  const menu = document.querySelector('.menu-nav');
+  sub_menu.addEventListener('click',()=>{
+    sub_menu.classList.toggle('isactive');
+    menu.classList.toggle('isactive');   
+});
+
+
